bf204673988f566e2a62b251ebdd80c3ae2b70ef
Richard McGhee <richardjmcghee@yahoo.com>
2021-09-06 20:57:56 -0400

    Added some more placeholder setup and comments inthe components I created. Setup the import for the wordcloud into the Experience component so there will be a nice cloud showing my skills and as a demonstration of previous work I've done.

---
+++ src/components/Experience.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import WordCloud from './wordCloud/WordCloud'
+
+function Experience() {
+    return ( 
+        <div>
+            <h1>Placeholder</h1>
+            <WordCloud />
+        </div>
+     );
+}
+
+export default Experience
---
+++ src/components/wordCloud/WordCloud.js
@@ -0,0 +1,18 @@
+import React, {useState, useEffect} from 'react'
+
+function WordCloud() {
+    // I'll have my skills randomly generate to form a wordcloud with
+    const [skills, setSkills] = useState([])
+    // This effect will be used for rendering the cloud and changing it based on the skills state
+    // useEffect(() =>{
+
+    // })
+
+    return ( 
+        <div>
+            <h1>Placeholder</h1>
+        </div>
+    )
+}
+
+export default WordCloud
