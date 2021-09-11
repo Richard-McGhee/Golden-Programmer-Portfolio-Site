c337e958422aa6ca2501a11a6f99e0287c1ee61b
Richard McGhee <richardjmcghee@yahoo.com>
2021-09-10 23:58:17 -0400

    Set up a .env file to set my browser to Chrome for testing and development. Set up the basics for the routing and links within the App component. Links will be going to Projects, Experience, and Resume page. I may rename Experience to Work Experience.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from 'react'
+import { Route, Link, Switch } from 'react-router-dom'
 import logo from './logo.svg'
 import './App.css'
 import Home from './components/Home'
@@ -11,14 +13,14 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav>
+          <Link to="/projects" className="navLinks">Projects</Link>
+          <Link to="/experience" className="navLinks">Work Experience</Link>
+          <Link to="/resume" className="navLinks">Resume</Link>
+        </nav>
+        <Switch>
+          {/* <Route exact path="/" component={Home} /> */}
+        </Switch>
       </header>
     </div>
   )
