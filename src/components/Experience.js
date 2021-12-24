12d8cc43c8cabb51ff810c74d8268054940891b9
Richard McGhee <richardjmcghee@yahoo.com>
2021-12-23 20:13:00 -0500

    Adjusted styling for experience page used styled-components. Everything is looking nice, just need to adjust for extra bullet points next; this will affect the Projects component as well.

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,6 +1,34 @@
 import React from 'react'
-// import WordCloud from './wordCloud/WordCloud'
+import styled from 'styled-components';
 
+const ExperienceStyles = styled.div`
+    .titleDiv{
+        width: 15%;
+        display: flex;
+        flex-wrap: wrap;
+        align-items: center;
+        justify-content: space-evenly;
+    }
+    p{
+        margin: 0 auto;
+        padding: 25px 0;
+        width: 60%;
+        font-size: 2rem;
+        text-align: center;
+        border: 2px dotted whitesmoke;
+    }
+    .expH1{
+        margin: 20px auto;
+        width: 30%;
+        border: 2px solid whitesmoke;
+    }
+    .pvtExpDiv, .mltryExpDiv{
+        font-size: 1.5rem;
+    }
+    .expTitles{
+        font-weight: bold;
+    }
+`
 function Experience() {
     const pvtExp = [
         {Company: "Integrity Restoration", Title: "Lead Restoration Technician", Dates: "Sep 2019 – Mar 2020", Job: "Performed tasks related to alleviating water damage and mold remediation", Job2: "Supervised temp employees", Job3: "Documented daily notes of activities performed to present to insurance companies", Job4: "Operated a company box van and managed equipment loading and removal"},
@@ -12,29 +40,33 @@ function Experience() {
         {USMC: "Ground Training", Title: "Database Manager", Job: "Coordinated air space and flight times for American and Japanese military officials", Job2: "Provided various other services for military personnel", Job3: "Recorded training statistics created training networks/programs for 200+ Marines"}
     ]
     return ( 
-        <div>
-            <h1>Experience</h1>
-            <h2>Richard McGhee</h2>
-            <h1>Private Experience</h1>
+        <ExperienceStyles>
+            <div className="titleDiv">
+                <h1>Experience</h1>
+                <h2>Richard McGhee</h2>
+            </div>
+            <p className="expIntro">
+                I've learned much in many areas over the years.< br/> Here's a quick breakdown of the most relevant areas I've worked.
+            </p>
+            <h1 className="expH1">Private Experience</h1>
             {pvtExp.map(exp => (
-                <div>
-                    <div>Company: {exp.Company}</div>
-                    <div>Title: {exp.Title}</div>
+                <div className="pvtExpDiv">
+                    <div className="expTitles">Company: {exp.Company}</div>
+                    <div className="expTitles">Title: {exp.Title}</div>
                     <div>Dates: {exp.Dates}</div>
                     <div>- {exp.Job} <br /> - {exp.Job2}</div><br />
                 </div>
             ))}
             <br />
-            <h1>Military Experience</h1>
+            <h1 className="expH1">Military Experience</h1>
             {mltryExp.map(exp => (
-                <div>
-                    <div>Job: {exp.USMC}</div>
-                    <div>Title: {exp.Title}</div>
+                <div className="mltryExpDiv">
+                    <div className="expTitles">Job: {exp.USMC}</div>
+                    <div className="expTitles">Title: {exp.Title}</div>
                     <div>- {exp.Job}<br />- {exp.Job2}<br />- {exp.Job3}</div><br />
                 </div>
             ))}
-            {/* <WordCloud /> */}
-        </div>
+        </ExperienceStyles>
      );
 }
 
